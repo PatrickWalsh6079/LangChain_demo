43ab5c727e1707dba6f1abfe800ad044eac2969f
Patrick <patrick.walsh6079@protonmail.com>
2023-06-18 21:45:15 -0400

    First commit

---
+++ application/models/bot.py
@@ -0,0 +1,12 @@
+
+from PyPDF2 import PdfReader
+from langchain import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import openai
+import os
+import secret_key
+
+llm = OpenAI(openai_api_key=secret_key.openai_api_key, model_name='text-davinci-003')
